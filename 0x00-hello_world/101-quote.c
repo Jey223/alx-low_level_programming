0bfe5b51a8f045fb88ebd292b38c3b54d6eb3a9c
Jey223 <julieudoh7@gmail.com>
2022-03-14 11:02:28 -0700

    How to get a standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Write out standardoutput (stdout)
+ * main - Write to stdout
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-
 	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
