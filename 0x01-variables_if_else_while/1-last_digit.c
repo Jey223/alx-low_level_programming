05f7fedafe8dfd12d23aeae5ad54ac558aabbefb
Jey223 <julieudoh7@gmail.com>
2022-03-16 09:33:39 -0700

    Getting the last digit of random numbers update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Checking Last number in random numbers
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n;
-	int lastdigit;
+	int n, lastdigit;
 
 	srand(time(0));
 	n = rand() -  RAND_MAX / 2;
-
 	lastdigit = n % 10;
-
+	printf("Last digit of %d is %d and is ", n, lastdigit);
 	if (lastdigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+		printf("greater than 5\n");
 	}
 	else if (lastdigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("0\n");
 	}
 	else if (lastdigit < 6 && lastdigit != 0)
 	{
-		printf("Last digit of %d is %d and is less thaan 6 and not 0\n", n, lastdigit);
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 }
