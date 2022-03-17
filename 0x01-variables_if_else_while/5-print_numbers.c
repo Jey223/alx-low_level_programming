af9bbf590b94083dc40adf1a3b842f4aef0d565b
Jey223 <julieudoh7@gmail.com>
2022-03-17 08:49:29 +0000

    Print out all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Printing numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i < 10)
+	{
+		printf("%d\n", i);
+
+		i++;
+	}
+	return (0);
+}
