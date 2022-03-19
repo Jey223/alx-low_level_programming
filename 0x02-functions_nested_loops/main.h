87c91ffe31c0b504ee00b103e1b6aee768f5803a
Jey223 <julieudoh7@gmail.com>
2022-03-19 05:08:54 +0000

    Using _putchar to print

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+/**
+*file: main.h
+*auth: Julie Udoh
+*desc: Header file contains prototype for all functions used in the 0x02-functions_nested_loops directory
+*/
+
+int _putchar(char);
+
+#endif
