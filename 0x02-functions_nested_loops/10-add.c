956db3c89ea42c2ee5f2984abf5a4b63fdf92633
Jey223 <julieudoh7@gmail.com>
2022-03-21 16:05:24 +0000

    Add teo integers and Returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*add - Function that add two integers a and b and returns the result
+*@a: first integer to be added
+*@b: second integer to be added
+*Return: sum
+*/
+
+int add(int a, int b)
+{
+
+	int sum;
+
+
+	sum = a + b;
+	return (sum);
+}
