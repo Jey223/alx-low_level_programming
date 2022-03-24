a32ecd23eb742fd38d21680fecba647c531d0e83
Jey223 <julieudoh7@gmail.com>
2022-03-24 22:57:39 +0000

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN
+#define MAIN
+
+/**
+*file: main.h
+*auth: Julie Udoh
+*desc: Header file contains all prototypes used in 0x06-pointers_arrays_strings
+*/
+
+char *_strcat(char *dest, char *src);
+#endif
