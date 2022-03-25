ff9a715bdaac1be20d4451a143c78cab9ac0a845
Jey223 <julieudoh7@gmail.com>
2022-03-25 02:52:03 +0000

    Concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,7 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
+
 	i = 0;
 	j = 0;
 
@@ -22,7 +23,7 @@ char *_strcat(char *dest, char *src)
 		j++;
 		i++;
 	}
-		dest[i] = '\0';
+	dest[i] = '\0';
 
-		return (0);
+	return (dest);
 }
