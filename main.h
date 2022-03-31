3e09759a26ab6a7293e19fe5dbe8ebf731fbffa4
Jey223 <julieudoh7@gmail.com>
2022-03-31 16:16:39 +0000

    C is upper 1, c is lower 0

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_F
+#define MAIN_F
+
+/**
+*file: main.h
+*auth: Julie Udoh
+*desc: Header file contains prototype for all functions used in the 0x04-more_functions_nested_loops directory
+*/
+
+int _isupper(int c);
+
+
+
+
+
+#endif
