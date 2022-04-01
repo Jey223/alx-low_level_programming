1e3e119757887f302a1847eee8014d3e10aff6ea
Jey223 <julieudoh7@gmail.com>
2022-04-01 09:04:43 +0000

    C-more functions

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - function
+*
+*Return: Always 0;
+*/
+
+int main(void)
+{
+	long i, num = 612852475143;
+
+	for (i = 1; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
